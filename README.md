10e848e4aadaed5d523333ff5be7879540dd8930
joneivison13 <joneivison355@gmail.com>
2020-10-02 18:54:43 -0300

    add README.md and database config

---
+++ README.md
@@ -0,0 +1,21 @@
+# DevHelp
+
+### Criado por Joneivison Oliveira, João Paulo Medeiros, Kaio Felipe e Aislânio Amorin.
+
+## Como iniciar o Banco de dados?
+
+1. crie um arquivo chamado ```.env``` na raiz do projeto.
+2. adicone nele 
+```
+DATABASE_HOST= '<host do banco de dados>'
+DATABASE_USERNAME='<usuário do banco de dados>'
+DATABASE_PASSWORD='<senha do banco de dados>'
+```
+**OBS** troque os **<>** pela informação que se pede.
+
+## Como iniciar o back-end?
+
+1. Instale o **node.js** pelo link https://node.org/  
+2. Com o nodejs instalado, entre na pasta do projeto eplo seu terminal.
+3. Estando na pasta do projeto aberta no terminal, execute o comando ```npm install```, em seguida  ```npm run build```, e por fim ```npm run start```.
+#### Backend Rodando com sucesso!
---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+require('dotenv').config()
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      database: 'DevHelp',
+      user:     process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD
+    },
+    migrations: {
+      directory: './src/database/migrations/'
+    },
+  },
+
+
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
